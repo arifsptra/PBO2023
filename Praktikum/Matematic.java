26c96b6429ef99c1259042b04d526e362541569e
arifsptra <arif.sptrra@gmail.com>
2023-03-16 09:03:12 +0700

    make program calculate two value

---
+++ Praktikum/Matematic.java
@@ -0,0 +1,35 @@
+public class Matematic {
+    void pertambahan(int value1, int value2) {
+        int result = value1+value2;
+        System.out.println("Pertambahan: " + value1 + " + " + value2 + " = " + result);
+    }
+    void pengurangan(int value1, int value2) {
+        int result = value1-value2;
+        System.out.println("Pengurangan: " + value1 + " - " + value2 + " = " + result);
+    }
+    void perkalian(int value1, int value2) {
+        int result = value1*value2;
+        System.out.println("Perkalian: " + value1 + " * " + value2 + " = " + result);
+    }
+    void pembagian(int value1, int value2) {
+        int result = value1/value2;
+        System.out.println("Pembagian: " + value1 + " / " + value2 + " = " + result);
+    }
+
+    void pertambahan(float value1, float value2) {
+        float result = value1+value2;
+        System.out.println("Pertambahan: " + value1 + " + " + value2 + " = " + result);
+    }
+    void pengurangan(float value1, float value2) {
+        float result = value1-value2;
+        System.out.println("Pengurangan: " + value1 + " - " + value2 + " = " + result);
+    }
+    void perkalian(float value1, float value2) {
+        float result = value1*value2;
+        System.out.println("Perkalian: " + value1 + " * " + value2 + " = " + result);
+    }
+    void pembagian(float value1, float value2) {
+        float result = value1/value2;
+        System.out.println("Pembagian: " + value1 + " / " + value2 + " = " + result);
+    }
+}
---
+++ Praktikum/MatematicDemo.java
@@ -0,0 +1,9 @@
+public class MatematicDemo {
+    public static void main(String[] args) {
+        Matematic math = new Matematic();
+        math.pertambahan(20, 20);
+        math.pengurangan(10, 5);
+        math.perkalian(10, 20);
+        math.pembagian(20, 2);
+    }
+}
