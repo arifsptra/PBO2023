0c9c974faa219d184e68889d27e11ab51f820106
arifsptra <arif.sptrra@gmail.com>
2023-03-30 09:53:28 +0700

    update program add scan input

--- Praktikum/Factorial.java
+++ Praktikum/Factorial.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
-        factorial(5);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bilangan    : ");
+        factorial(input.nextInt());
     }
 
     static void factorial(int bil) {
         int temp=1;
-        System.out.println("Bilangan    : " + bil);
         for(int i=1; i<bil; i++) {
             System.out.print(temp + " x " + (i+1) + " = ");
             temp = temp*(i+1);
