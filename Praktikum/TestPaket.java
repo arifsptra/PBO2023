6e5edf30a41dd099c396a088b679fc5b6f7a137a
arifsptra <arif.sptrra@gmail.com>
2023-04-17 12:30:46 +0700

    practikum package in java

---
+++ Praktikum/TestPaket.java
@@ -0,0 +1,12 @@
+import kelasku.Budi;
+import kelasku.Joko;
+
+public class TestPaket {
+    public static void main(String[] args) {
+        Budi objectB = new Budi();
+        Joko objectJ = new Joko();
+
+        objectB.info();
+        objectJ.info();
+    }
+}
