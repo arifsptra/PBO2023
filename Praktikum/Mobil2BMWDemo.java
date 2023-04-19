a0be21cc62a5c48ac01a78e7d6e4d7ead4fff37f
arifsptra <arif.sptrra@gmail.com>
2023-04-19 21:10:03 +0700

    exercise 1 in week 7

---
+++ Praktikum/Mobil2BMWDemo.java
@@ -0,0 +1,10 @@
+public class Mobil2BMWDemo {
+    public static void main(String[] args) {
+        Mobil2BMW bmw = new Mobil2BMW();
+
+        bmw.hidup("BMW");
+        bmw.nontonTV();
+        bmw.ubahGigi("20");
+        bmw.mati("BMW");
+    }
+}
