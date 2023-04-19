90fc19874fbd46c2ae4a288d385d66059d0c36b6
arifsptra <arif.sptrra@gmail.com>
2023-04-19 21:01:13 +0700

    learn basic inheritance

---
+++ Praktikum/MountainBikeDemo.java
@@ -0,0 +1,8 @@
+public class MountainBikeDemo {
+    public static void main(String[] args) {
+        MountainBike mBike = new MountainBike();
+        mBike.speedUp(10);
+        mBike.changeGear(2);
+        mBike.setHeight(20);
+    }
+}
