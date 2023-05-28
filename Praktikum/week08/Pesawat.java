ad7baa0b05b2bbf90ec7fbefc5c506c65fa93564
arifsptra <arif.sptrra@gmail.com>
2023-05-29 00:18:00 +0700

    do a practical two of polymorphism

---
+++ Praktikum/week08/Pesawat.java
@@ -0,0 +1,15 @@
+public class Pesawat {
+    String sayap, ekor;
+    
+    Pesawat(){
+        System.out.println("object Pesawat dibuat............");
+    }
+
+    void terbang(){
+        System.out.println("Terbang..........");
+    }
+
+    void mendarat(){
+        System.out.println("Mendarat..........");
+    }
+}
